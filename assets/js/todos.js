4d13083db81e17711fa1b7ed4f0781dcd5dede80
Haly Yampolska <halyna.yampolska@gmail.com>
2023-01-04 18:31:21 +0200

    1. Check Off Specific Todos By Clicking 2. Click on X to delite Todo 3. Build Input Forms

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,40 @@
-alert("CONNECTED!");
+//Check Off Specific Todos By Clicking 
+$("ul").on("click", "li", function(){
+    /*
+    //if li is grey
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        //turn it black
+        $(this).css({
+            color: "black",
+            textDecoration: "none",   
+        });
+    }
+        //else 
+        else {
+        //turn it gray
+            $(this).css({
+                color: "grey",
+                textDecoration: "line-through",   
+            });
+    }*/
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delite Todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+   event.stopPropagation(); 
+});
+
+//Build Input Forms 
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        //grabbing new todo text from input 
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul 
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
